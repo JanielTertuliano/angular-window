745be3611ad7a4c7d606dddb25187ef8da7248a9
Janiel <janiel.web@gmail.com>
2019-08-13 11:56:37 -0300

    remove jqwidgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { jqxBarGaugeComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbargauge';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    jqxBarGaugeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
