7c9e40684444bd641d548b5efabba1155c2771a2
Janiel <janiel.web@gmail.com>
2019-08-13 16:43:01 -0300

    Add jquerry and window initial creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WindowComponent } from './window/window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WindowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+declare let $: any;
 
 @Component({
   selector: 'app-window',
   templateUrl: './window.component.html',
-  styleUrls: ['./window.component.css']
+  styleUrls: ['./window.component.scss']
 })
 export class WindowComponent implements OnInit {
+
+  idElement: string = '#draggable';
+  maximumSize: boolean;
+  miniSize: boolean;
+  valor: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.initWindow();
+  }
+
+  initWindow() {
+    $(() => {
+      $(this.idElement).draggable();
+    });
+
+    $(() => {
+      $(this.idElement).resizable({
+        minHeight: 200,
+        minWidth: 200,
+      });
+    });
+  }
+
+  maximize() {
+    this.maximumSize = true;
+    $(this.idElement).addClass('maximized');
+    $(this.idElement).draggable('disable');
+    $(this.idElement).resizable('disable');
+  }
+
+  resizable() {
+    this.maximumSize = false;
+    $(this.idElement).removeClass('maximized');
+    $(this.idElement).draggable('enable');
+    $(this.idElement).resizable('enable');
+  }
+
+  minimize() {
+    this.miniSize = true;
+    $(this.idElement).draggable('disable');
+    $(this.idElement).resizable('disable');
+  }
+
+  undoMinimize() {
+    this.miniSize = false;
+    this.initWindow();
+  }
+
+  closeWindow() {
+    $(this.idElement).remove();
   }
 
 }
