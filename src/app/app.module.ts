836e2800ead0951d2187c26daab5c2928ab869c7
Janiel <janiel.web@gmail.com>
2019-08-13 11:11:44 -0300

    Config jqwidgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { jqxBarGaugeComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbargauge';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    jqxBarGaugeComponent
   ],
   imports: [
     BrowserModule
