6b574bb3fb375046c562ebc72640315e38407518
Janiel <janiel.web@gmail.com>
2019-08-26 10:16:14 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-window'));
 
-app.get('/*', function(req,res) {
-    
+app.get('/*', (req,res) => {
+
 res.sendFile(path.join(__dirname+'/dist/angular-window/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Node executando na porta 8080');
+});
